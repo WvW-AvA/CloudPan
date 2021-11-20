bc95ee1e664463fce637b055d15c0bf7c4050e52
kimo <455300672@qq.com>
2021-11-20 21:46:29 +0800

    modified:   CloudPan.cpp

--- CloudPan.cpp
+++ CloudPan.cpp
@@ -5,5 +5,15 @@ using namespace std;
 
 int main(int argc,char * argv[])
 {
+    MYSQL mysql;
+    MYSQL *connect;
+    if(mysql_library_init(0,NULL,NULL))
+    {
+        cout<<"could not initialize MySQL Library\n";
+        return 1;
+    }
+    mysql_init(&mysql);
+    //connect=mysql_real_connect()
+
     std::cout<<"aaaa";
 }
